ae2f9555e37e94d8cdb5864f7d10689d574f2de0
jesusrpd <jesusrodrigo881@gmail.com>
2022-07-15 13:50:26 -0500

    integración de graphql

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import authRoute from './router/auth.routes';
 import helloRoute from './router/welcome.routes';
+import {ApolloServer, gql} from 'apollo-server';
 const app = express();
 
 app.use(cors());
@@ -12,5 +13,62 @@ app.use(morgan("dev"));
 
 app.use('/auth', authRoute);
 app.use('/welcome', helloRoute);
+
+// Types, DATES Y RESOLVERS TEMPORALES
+let persons = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+  },
+  {
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki",
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+  },
+  {
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki",
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+  },
+]
+
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String! 
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) =>
+      persons.find(p => p.name === args.name)
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs, resolvers
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
 
 export default app;
